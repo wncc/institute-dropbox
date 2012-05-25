b6656e7157d57b90d3c1240b4c8bd1b39e55469c
Viraj <virajsawa@gmail.com>
2012-05-25 21:30:58 +0530

    basic

---
+++ basic_ftp.py
@@ -0,0 +1,12 @@
+global username,password
+import getpass
+print "Enter your LDAP id and Password \n"
+username= raw_input('Username : ')
+password= getpass.getpass('Password : ')
+
+from ftplib import FTP
+global ftp
+ftp = FTP ('home.iitb.ac.in')
+ftp.login(username,password)
+print "Welcome "+ username + " to your bighome home folder"
+
